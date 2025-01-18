d3edef0183b2f822dac6016949a72e356de28a50
Hunter <i.am.hunter.777.ih@gmail.com>
2025-01-18 08:59:20 +0300

    Added and updated 'App'

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,64 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div class="app">
+    <h1>Список Книг</h1>
+    <SearchInput @search="handleSearch" />
+    <BookList :books="filteredBooks" />
+    <BookForm @book-added="handleBookAdded" />
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<script setup>
+import { ref, computed } from 'vue'
+import BookList from './components/BookList.vue'
+import BookForm from './components/BookForm.vue'
+import SearchInput from './components/SearchInput.vue'
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+const books = ref([
+  {
+    title: 'Как разговаривать с кем угодно, когда угодно, где угодно',
+    author: 'Ларри Кинг',
+    year: 2011,
+    genre: 'Научпоп',
+  },
+  {
+    title: 'Больше, чем просто красивая. 12 тайных сил женщины, перед которой невозможно устоять',
+    author: 'Кара Кинг',
+    year: 2020,
+    genre: 'Научпоп',
+  },
+  {
+    title:
+      'Искусство системного мышления. Необходимые знания о системах и творческом подходе к решению проблем',
+    author: "Джозеф О'Коннор и Иан Макдермотт",
+    year: 2018,
+    genre: 'Научпоп',
+  },
+])
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
+const searchQuery = ref('')
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+const filteredBooks = computed(() => {
+  if (!searchQuery.value) {
+    return books.value
   }
+  return books.value.filter((book) =>
+    book.title.toLowerCase().includes(searchQuery.value.toLowerCase()),
+  )
+})
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+const handleBookAdded = (newBook) => {
+  books.value.push(newBook)
+}
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+const handleSearch = (query) => {
+  searchQuery.value = query
+}
+</script>
 
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+<style lang="scss">
+.app {
+  max-width: 1024px;
+  margin: 0 auto;
+  padding: 40px;
 }
 </style>
