7773062a481f965eb5cf4395a86975c35c2905af
Hunter <i.am.hunter.777.ih@gmail.com>
2025-01-18 09:01:48 +0300

    Added new component 'BookList'

---
+++ src/views/BookList.vue
@@ -0,0 +1,23 @@
+<template>
+  <ul class="book-list">
+    <BookItem v-for="book in books" :key="book.title" :book="book" />
+  </ul>
+</template>
+
+<script setup>
+import BookItem from '@/components/BookItem.vue'
+
+defineProps({
+  books: {
+    type: Array,
+    required: true,
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.book-list {
+  list-style: none;
+  padding: 0;
+}
+</style>
