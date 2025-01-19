dd37a2f8f73fcdd601129ce60e0a86270c88e52e
Hunter <i.am.hunter.777.ih@gmail.com>
2025-01-19 07:33:48 +0300

    Added new component 'SeschInput'

---
+++ src/components/SearchInput.vue
@@ -0,0 +1,30 @@
+<template>
+  <input
+    type="text"
+    placeholder="Найти ту самую книгу"
+    v-model="searchQuery"
+    @input="handleInput"
+  />
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const searchQuery = ref('')
+
+const emit = defineEmits(['search'])
+
+const handleInput = () => {
+  emit('search', searchQuery.value)
+}
+</script>
+
+<style lang="scss" scoped>
+input {
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  margin-bottom: 20px;
+  width: 100%;
+}
+</style>
