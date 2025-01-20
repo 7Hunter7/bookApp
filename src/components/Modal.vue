39eba90129a165e0088dc3bad482d511231dc5ff
Hunter <i.am.hunter.777.ih@gmail.com>
2025-01-20 20:43:32 +0300

    Added new component 'Modal'

---
+++ src/components/Modal.vue
@@ -0,0 +1,70 @@
+<template>
+  <div v-if="isOpen" class="modal-overlay">
+    <div class="modal">
+      <div class="modal-header">
+        <slot name="header">
+          <h2>{{ title }}</h2>
+        </slot>
+        <button class="close-button" @click="$emit('close')">
+          <img :src="Close" alt="close" />
+        </button>
+      </div>
+      <div class="modal-body">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import Close from '/icons/close-grey.svg'
+
+defineProps({
+  isOpen: {
+    type: Boolean,
+    default: false,
+  },
+  title: {
+    type: String,
+    default: 'Модальное окно',
+  },
+})
+
+defineEmits(['close'])
+</script>
+
+<style lang="scss" scoped>
+.modal-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1000;
+}
+
+.modal {
+  background-color: #2a2a2a;
+  padding: 20px;
+  border-radius: 8px;
+  max-width: 600px;
+  width: 100%;
+}
+
+.modal-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+
+.close-button {
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+}
+</style>
