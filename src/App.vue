82d5ce663b17632a153f44105956ca90268a2149
Hunter <i.am.hunter.777.ih@gmail.com>
2025-01-20 20:54:56 +0300

    Updated 'App' - Added import and usage new component 'Modal'

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,58 @@
     <SearchInput @search="handleSearch" />
     <BookList :books="filteredBooks" />
     <BookForm @book-added="handleBookAdded" />
+    <Modal :isOpen="isModalOpen" title="Редактирование книги" @close="closeModal">
+      <template #header>
+        <h2>
+          <Icon name="edit" />
+          Редактирование книги
+        </h2>
+      </template>
+      <form v-if="currentBook" class="book-form modal-book-form" @submit.prevent="saveBook">
+        <div>
+          <label for="title">Название</label>
+          <input type="text" id="title" v-model="currentBook.title" required />
+        </div>
+        <div>
+          <label for="author">Автор</label>
+          <input type="text" id="author" v-model="currentBook.author" required />
+        </div>
+        <div>
+          <label for="year">Год</label>
+          <input type="number" id="year" v-model.number="currentBook.year" />
+        </div>
+        <div>
+          <label for="genre">Жанр</label>
+          <input type="text" id="genre" v-model="currentBook.genre" />
+        </div>
+        <div class="modal-actions">
+          <button class="save-button" type="submit"><Icon name="save" /> Сохранить</button>
+          <button class="delete-button" type="button" @click="deleteBook">
+            <Icon name="delete" /> Удалить
+          </button>
+          <div class="checkbox">
+            <input type="checkbox" id="confirm" v-model="agreed" />
+            <label for="confirm"
+              >Я согласен с условиями <a href="">Политики конфиденциальности</a></label
+            >
+          </div>
+        </div>
+      </form>
+    </Modal>
+
+    <Modal :isOpen="isDeleteModalOpen" title="Подтверждение удаления" @close="closeDeleteModal">
+      <template #header>
+        <h2>
+          <Icon name="delete" />
+          Подтверждение удаления
+        </h2>
+      </template>
+      <p>Вы действительно хотите удалить книгу "{{ currentBook?.title }}"?</p>
+      <div class="modal-actions">
+        <button class="confirm-delete-button" @click="confirmDelete">Да</button>
+        <button @click="closeDeleteModal">Нет</button>
+      </div>
+    </Modal>
   </div>
 </template>
 
@@ -12,6 +64,7 @@ import { ref, computed } from 'vue'
 import BookList from '@/views/BookList.vue'
 import BookForm from '@/components/BookForm.vue'
 import SearchInput from '@/components/SearchInput.vue'
+import Modal from '@/components/Modal.vue'
 
 const books = ref([
   {
@@ -36,6 +89,10 @@ const books = ref([
 ])
 
 const searchQuery = ref('')
+const isModalOpen = ref(false)
+const isDeleteModalOpen = ref(false)
+const currentBook = ref(null)
+const agreed = ref(false)
 
 const filteredBooks = computed(() => {
   if (!searchQuery.value) {
@@ -53,11 +110,114 @@ const handleBookAdded = (newBook) => {
 const handleSearch = (query) => {
   searchQuery.value = query
 }
+
+const openEditModal = (book) => {
+  currentBook.value = book
+  isModalOpen.value = true
+  agreed.value = false
+}
+
+const openDeleteModal = (book) => {
+  currentBook.value = book
+  isDeleteModalOpen.value = true
+}
+
+const closeModal = () => {
+  isModalOpen.value = false
+  currentBook.value = null
+}
+
+const closeDeleteModal = () => {
+  isDeleteModalOpen.value = false
+  currentBook.value = null
+}
+
+const saveBook = () => {
+  if (!agreed.value) {
+    alert('Необходимо принять условия')
+    return
+  }
+  const index = books.value.findIndex((book) => book.title === currentBook.value.title)
+  if (index !== -1) {
+    books.value[index] = { ...currentBook.value }
+  }
+  closeModal()
+}
+
+const deleteBook = () => {
+  isModalOpen.value = false
+  isDeleteModalOpen.value = true
+}
+
+const confirmDelete = () => {
+  const index = books.value.findIndex((book) => book.title === currentBook.value.title)
+  if (index !== -1) {
+    books.value.splice(index, 1)
+  }
+  closeDeleteModal()
+}
 </script>
 
 <style lang="scss">
 h1 {
   text-align: center;
   margin-bottom: 20px;
+}
+
+.modal-book-form {
+  padding: 0;
+  background-color: transparent;
+}
+.modal-actions {
+  display: flex;
+  justify-content: flex-end;
+  gap: 10px;
+  margin-top: 20px;
+  align-items: center;
+}
+
+.save-button {
+  background-color: #28a745;
+  color: white;
+  border: none;
+  padding: 10px 15px;
+  border-radius: 5px;
+  cursor: pointer;
+  display: inline-flex;
+  align-items: center;
+  gap: 5px;
+}
+
+.delete-button {
+  color: white;
+  border: 1px solid #dc3545;
+  padding: 10px 15px;
+  border-radius: 5px;
+  cursor: pointer;
+  background-color: transparent;
+  display: inline-flex;
+  align-items: center;
+  gap: 5px;
+}
+
+.confirm-delete-button {
+  background-color: #dc3545;
+  color: white;
+  border: none;
+  padding: 10px 15px;
+  border-radius: 5px;
+  cursor: pointer;
+  display: inline-flex;
+  align-items: center;
+  gap: 5px;
+}
+.checkbox {
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  input {
+    width: 20px;
+    height: 20px;
+  }
 }
 </style>
